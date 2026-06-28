f35514df16e2673a84a1f60cf6226790c2cdcd18
XUE2026 <XUE2026@users.noreply.github.com>
2026-06-28 07:32:38 +0000

    fix(android): 修复 Rust JNI 编译错误
    - 使用 unsafe JString::from_raw 转换原始 jstring 指针
    - 替换已移除的 into_inner() 为 drop(s)

--- ClassIsland.Android/rust_core/src/lib.rs
+++ ClassIsland.Android/rust_core/src/lib.rs
@@ -1,5 +1,5 @@
 use jni::JNIEnv;
-use jni::objects::JClass;
+use jni::objects::{JClass, JString};
 use jni::sys::{jboolean, jint, jstring, JNI_TRUE};
 
 #[no_mangle]
@@ -7,9 +7,9 @@ pub extern "system" fn Java_com_classisland_android_rust_GpuAccelerator_nativeIn
 
 #[no_mangle]
 pub extern "system" fn Java_com_classisland_android_rust_GpuAccelerator_nativeRender(
-    mut env: JNIEnv, _class: JClass, items_json: JString, _width: jint, _height: jint, _bg: jint, _fg: jint
+    mut env: JNIEnv, _class: JClass, items_json: jstring, _width: jint, _height: jint, _bg: jint, _fg: jint
 ) -> jboolean {
-    if let Ok(s) = env.get_string(&items_json) { let _ = s.into_inner(); }
+    if let Ok(s) = unsafe { env.get_string(&JString::from_raw(items_json)) } { drop(s); }
     JNI_TRUE
 }
 
